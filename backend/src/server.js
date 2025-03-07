ed3d02d1da15de657d44d39998a3f0d626af97c4
Luan Drulla <serighelli003@gmail.com>
2025-03-07 00:12:11 -0300

    updates

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", routes);
+app.use("/", routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
