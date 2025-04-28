36c007476a6c888d28e7cc842ac74a451dcf45d8
Luan Drulla <serighelli003@gmail.com>
2025-04-28 20:55:30 -0300

    Ajuste fly discord bot

---
+++ discord-bot/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Bot HTTP server rodando na porta ${PORT}`);
+});
